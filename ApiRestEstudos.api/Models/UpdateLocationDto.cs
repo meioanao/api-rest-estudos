16db050ca4f5252caa0f84bc21cd3fd9ab1441a9
meioanao <matheusfaria04@gmail.com>
2026-05-30 23:12:01 -0300

    docker support

--- ApiRestEstudos.api/Models/UpdateLocationDto.cs
+++ ApiRestEstudos.api/Models/UpdateLocationDto.cs
@@ -1,7 +1,9 @@
-﻿public class UpdateLocationDto
+﻿namespace ApiRestEstudos.Api.Dtos;
+
+public class UpdateLocationDto
 {
-    public string Nome { get; set; }
-    public string Categoria { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Categoria { get; set; } = string.Empty;
     public double Latitude { get; set; }
     public double Longitude { get; set; }
 }
--- ApiRestEstudos.api/Program.cs
+++ ApiRestEstudos.api/Program.cs
@@ -3,17 +3,21 @@ using ApiRestEstudos.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql("Host=localhost;Port=5432;Database=locationsdb;Username=postgres;Password=12345678"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.MapControllers();
-
-// app.UseHttpsRedirection(); 
 
 app.Run();
