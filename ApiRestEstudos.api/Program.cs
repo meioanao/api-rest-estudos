0a2c93f4f951fecd6035e8c801aa5c5b2bc991b6
meioanao <matheusfaria04@gmail.com>
2026-05-30 23:35:13 -0300

    update

--- ApiRestEstudos.api/Program.cs
+++ ApiRestEstudos.api/Program.cs
@@ -10,6 +10,9 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -17,6 +20,9 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
